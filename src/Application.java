5bea5b102966a63ccd9704aef9afc9f9a5bce00e
pierre-yves-fourmond <pierre-yves.fourmond@octo.com>
2025-07-26 15:12:25 +0200

    Simplification

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,4 @@
-///usr/bin/env java --enable-preview --class-path ${APP_DIR:-.}/lib/'*' "$0" "$@"; exit $?
+//usr/bin/env java --enable-preview --class-path ${APP_DIR:-.}/lib/'*' "$0" "$@"; exit $?
 
 void main() {
     String languageAsString = readln("Quelle langue parlez-vous / Which language do you speak (French / English) ? ");
